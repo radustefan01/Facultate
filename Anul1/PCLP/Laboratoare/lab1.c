8fbc1dbaf43aa969a17368acb1d9311a634f36bb
radustefan01 <radustefan411@gmail.com>
2024-10-19 00:45:46 +0300

    start

---
+++ Anul1/PCLP/Laboratoare/lab1.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+
+int factorial (int n)
+{
+    if(n < 0)
+        return -1;
+    int p, i;
+    for(i = 1; i <= n; ++i)
+        p *= i;
+    return i;
+}
+
+ int main(void)
+ {
+        
+    return 0;
+ }
